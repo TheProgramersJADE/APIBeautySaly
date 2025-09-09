80a2c84e2e29563644fe0ac22947a3dc27ba34b2
Emily Lisbeth Bolaño Flores <emilyflores.093@gmail.com>
2025-09-09 11:18:24 -0600

    IServicioRepository y ModelMapper

---
+++ src/main/java/org/esfe/repositorios/IServicioRepository.java
@@ -0,0 +1,9 @@
+package org.esfe.repositorios;
+
+import org.esfe.modelos.Servicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface IServicioRepository extends JpaRepository<Servicio, Integer> {
+
+}
